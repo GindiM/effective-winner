c93373f7a9e22fef5108f9f87af0689641106a30
GindiM <54244938+GindiM@users.noreply.github.com>
2019-10-02 18:55:18 +0300

    Add files via upload

---
+++ Switch abetween 3 number by all options.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp18
+{
+	class Program
+	{
+		static void Main(string[] args)
+		{
+
+			int[] arr = { 1, 2, 3 };
+			int temp;
+			int r = 2;
+
+
+
+
+			for (int l = 0; l < 3; l++)
+			{
+
+
+				for (int i = 0; i < 3; i++)
+				{
+
+
+
+					if (i != r)
+					{
+						temp = arr[r];
+						arr[r] = arr[i];
+						arr[i] = temp;
+						Console.Write(arr[0]);
+						Console.Write(arr[1]);
+						Console.WriteLine(arr[2]);
+					}
+
+
+				}
+
+
+
+
+
+
+
+
+
+
+
+			}
+		}
+	}
+}
