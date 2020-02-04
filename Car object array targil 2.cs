efcd50c8420a24e6882a79edf34bd9914d1f85f4
GindiM <54244938+GindiM@users.noreply.github.com>
2020-02-04 20:37:38 +0200

    Add files via upload

---
+++ Car object array targil 2.cs
@@ -0,0 +1,79 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp40
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Car[] car = new Car[6];
+
+            car[0] = new Car("Mitsubishi", "Lancer Evolution", 170000, true);
+            car[1] = null;
+            car[2] = new Car("Audi", "A7", 40000, true);
+            car[3] = car[2];
+            car[4] = car[2];
+            car[5] = new Car("Seniorita", "Blondanita with Nemashimos", 0, true);
+
+            PrintCar(car);
+
+
+
+        }
+        static void PrintCar(Car[] car)
+        {
+            for (int i = 0; i < car.Length; i++)
+            {
+                if (car[i] != null)
+                {
+                    car[i].Details();
+
+                }
+
+            }
+        }
+
+
+
+        class Car
+        {
+
+            public string manufacturer;
+            public string model;
+            public int km;
+            public bool abs;
+            public Car(string manufacturer, string model, int km, bool abs)
+            {
+                this.manufacturer = manufacturer;
+                this.model = model;
+                this.km = km;
+                this.abs = abs;
+            }
+            public void Details()
+            {
+                string Details = manufacturer + " " + model + " " + km.ToString() + " " + abs.ToString();
+                Console.WriteLine(Details);
+            }
+
+        }
+
+
+
+
+
+
+
+    }
+}
+
+
+
+
+
+
+
